12cc75f63a04ad86ddc22239d78e6407b80ea8fc
roluochKe <rasholuoch@gmail.com>
2021-12-19 00:23:21 +0300

    security using apikey and creating access.log

---
+++ server/src/utils/isAuthenticated.js
@@ -0,0 +1,18 @@
+require('dotenv/config')
+
+export const isAuthenticated = async (req, res, next) => {
+  if (!req.query.apiKey) {
+    return res.status(401).send({
+      message: 'Must be authenticated with an API Key to hit this endpoint',
+    })
+  } else {
+    const { apiKey } = req.query
+    if (apiKey === process.env.API_KEY) {
+      return next()
+    } else {
+      return res.status(401).send({
+        message: 'API Key does not match!',
+      })
+    }
+  }
+}
